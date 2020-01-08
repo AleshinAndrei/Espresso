5762c3a49a2dcfffa1e0cf5f4c29bc3fff4ba7e6
AleshinAndrei <aleshin_andrej@list.ru>
2020-01-08 16:33:41 +0300

    commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from PyQt5 import uic
+import sqlite3
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        con = sqlite3.connect('coffee.db')
+        cur = con.cursor()
+        res = cur.execute("SELECT * FROM varieties").fetchall()
+        self.table.setColumnCount(7)
+        self.table.setHorizontalHeaderLabels(
+            ['ID', 'name', "degree of\n\troasting", 'grinding', 'description', 'price', 'volume']
+        )
+        self.table.setRowCount(len(res))
+        for i, row in enumerate(res):
+            for j, elem in enumerate(row):
+                self.table.setItem(i, j, QTableWidgetItem(str(elem)))
+        self.table.resizeColumnsToContents()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
